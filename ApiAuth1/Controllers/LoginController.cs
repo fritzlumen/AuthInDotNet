bd6f52789b90b909a558b8a287d244038afe0652
fritzlumen <fritzlumen@gmail.com>
2021-11-12 09:11:37 -0300

    Primeiro commit

---
+++ ApiAuth1/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using ApiAuth1.Models;
+using ApiAuth1.Repositories;
+using ApiAuth1.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAuth1.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class LoginController : ControllerBase 
+    {
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> AuthenticateAsync([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.Username, model.Password);
+            if (user == null)
+            {
+                return NotFound(new { message = "Usuário ou senha inválidos"});
+            }
+
+            var token = TokenService.GenerateToken(user);
+
+            user.Password = "";
+
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
---
+++ ApiAuth1/Services/TokenService.cs
@@ -0,0 +1,34 @@
+﻿using ApiAuth1.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAuth1.Services
+{
+    public static class TokenService
+    {
+         public static string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
